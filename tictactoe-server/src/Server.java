ca30348f202e5b92d62cc49fedcd0daf95b8d3cc
MichalGrzymski9779 <32016393+MichalGrzymski9779@users.noreply.github.com>
2017-11-06 14:22:52 +0100

    Update Server.java
    server updated

--- tictactoe-server/src/Server.java
+++ tictactoe-server/src/Server.java
@@ -1,20 +1,32 @@
-
+import java.io.*;
+import java.net.*;
 public class Server {
-
+	static ServerSocket svSocket;
+	static Client cl1, cl2, cl3, cl4;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//hello world!
-		//Roberts test 11:14
-		//Test test
-<<<<<<< HEAD
-		//WOW 
-		//MI HOW how how
-=======
-		//WOW
-		//MI HOW
-		//Nihaw 2.0
-		
-		// testing maja more testinghsdfjhdsjfdsj 
->>>>>>> branch 'master' of https://github.com/loghinfrancisc/Programming-MiniProject
-	}
+try {
+	//Listener type socket, which can accept incoming connections and assign them to other sockets, allowing multiple connections
+	svSocket=new ServerSocket(7007);
+
+} catch (Exception e) {
+	e.printStackTrace();
+}
+	//Allows for 4 connections
+try {
+	cl1=new Client(svSocket.accept());
+	cl1.start();
+	cl2=new Client(svSocket.accept());
+	cl2.start();
+	cl3=new Client(svSocket.accept());
+	cl3.start();
+	cl4=new Client(svSocket.accept());
+	cl4.start();
+	
+}
+catch(Exception e) {
+	e.printStackTrace();
+}
+}
+
 }
